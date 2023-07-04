16d24cbbf4677cc168e280872350b3c5ef9d6274
William Mtemi Mponzi <willbrian009@gmail.com>
2023-07-04 13:09:35 +0300

    resource generation and stating relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeding...."
+heros = Hero.create([{name: 'Kamala Khan', super_name: 'Ms. Marvel'},
+                    { name: "Doreen Green", super_name: "Squirrel Girl" },
+                    { name: "Gwen Stacy", super_name: "Spider-Gwen" }])
+
+powers = Power.create([{ name: "super strength", description: "gives the wielder super-human strengths" },
+{ name: "flight", description: "gives the wielder the ability to fly through the skies at supersonic speed" }])
+
+hero_powers = HeroPower.create([{ strength: "Average", power_id: 1, hero_id: 1 },
+                                { strength: "Strong", power_id: 2, hero_id: 1 }])
+        
+puts "Seeding done"
